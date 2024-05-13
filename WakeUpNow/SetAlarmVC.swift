ace7c9bdce76fb176ddee96bbff477d99439ec6a
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-05-13 22:42:31 +0900

    소리 설정 화면 연결

--- WakeUpNow/SetAlarmVC.swift
+++ WakeUpNow/SetAlarmVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class SetAlarmVC: UIViewController {
     
@@ -14,51 +15,65 @@ class SetAlarmVC: UIViewController {
         
         view.backgroundColor = .white
         
-        // 네비게이션 바 타이틀 설정
         self.title = "알람 추가"
         
-        // 네비게이션 바 왼쪽에 '취소' 버튼 추가
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(cancelButtonTapped))
         
-        // 네비게이션 바 오른쪽에 '저장' 버튼 추가
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "저장", style: .plain, target: self, action: #selector(saveButtonTapped))
         
         setupNextButton()
+        setupSoundSettingButton() // '소리 설정' 버튼 설정 함수 호출
     }
     
     func setupNextButton() {
-        // 버튼 생성
         let nextButton = UIButton(type: .system)
         nextButton.setTitle("반복요일", for: .normal)
         nextButton.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
-        
-        // 버튼을 view에 추가
         view.addSubview(nextButton)
         
-        // 버튼의 위치와 크기 설정
-        nextButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            nextButton.widthAnchor.constraint(equalToConstant: 100),
-            nextButton.heightAnchor.constraint(equalToConstant: 50)
-        ])
+        // SnapKit을 사용한 제약 설정
+        nextButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(-30) // '소리 설정' 버튼과 겹치지 않도록 조정
+            make.width.equalTo(100)
+            make.height.equalTo(50)
+        }
+    }
+    
+    func setupSoundSettingButton() {
+        let soundSettingButton = UIButton(type: .system)
+        soundSettingButton.setTitle("소리 설정", for: .normal)
+        soundSettingButton.addTarget(self, action: #selector(soundSettingButtonTapped), for: .touchUpInside)
+        view.addSubview(soundSettingButton)
+        
+        // SnapKit을 사용한 제약 설정
+        soundSettingButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(30) // '반복요일' 버튼에 대해 상대적으로 위치 조정
+            make.width.equalTo(100)
+            make.height.equalTo(50)
+        }
     }
     
     @objc func nextButtonTapped() {
-        // 다음 화면으로 넘어가는 로직 구현
-        let nextVC = SetDayVC() // 다음 화면의 뷰 컨트롤러 인스턴스 생성
+        let nextVC = SetDayVC()
         self.navigationController?.pushViewController(nextVC, animated: true)
     }
     
-    // '취소' 버튼이 탭되었을 때 호출될 메서드
     @objc func cancelButtonTapped() {
         self.dismiss(animated: true, completion: nil)
     }
     
-    // '저장' 버튼이 탭되었을 때 호출될 메서드
     @objc func saveButtonTapped() {
-        // 여기에 알람 저장 로직 구현
         print("알람이 저장되었습니다.")
     }
+    
+    @objc func soundSettingButtonTapped() {
+        let nextVC = SetSoundVC()
+        self.navigationController?.pushViewController(nextVC, animated: true)
+        // '소리 설정' 버튼이 탭되었을 때의 로직 구현
+        print("소리 설정 화면으로 이동합니다.")
+        // 여기에 소리 설정 화면으로 이동하는 코드 추가
+    }
 }
+
