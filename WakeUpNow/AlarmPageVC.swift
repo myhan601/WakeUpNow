04f3ef1edd31b57ea040f17f73588485f3d9ae60
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-05-13 20:06:08 +0900

    테이블 뷰 기초 구현

--- WakeUpNow/AlarmPageVC.swift
+++ WakeUpNow/AlarmPageVC.swift
@@ -10,6 +10,8 @@ import SnapKit
 
 class AlarmPageVC: UIViewController {
     
+    var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,6 +22,24 @@ class AlarmPageVC: UIViewController {
         
         // 네비게이션 바의 오른쪽에 '+' 버튼 추가
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        
+        // 테이블뷰 설정 및 레이아웃
+        setupTableView()
+    }
+    
+    func setupTableView() {
+        // 테이블뷰 초기화 및 설정
+        tableView = UITableView()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        view.addSubview(tableView)
+        
+        // SnapKit을 사용하여 테이블뷰의 제약조건 설정
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
     
     // '편집' 버튼이 탭되었을 때 호출될 메서드
@@ -33,4 +53,24 @@ class AlarmPageVC: UIViewController {
         // 추가 관련 동작 구현
         print("'+' 버튼이 탭되었습니다.")
     }
+}
+
+extension AlarmPageVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 섹션당 행의 수를 5로 설정
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 셀을 재사용하거나 새로 생성
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .default, reuseIdentifier: "cell")
+        
+        // 셀 내용 설정
+        cell.textLabel?.text = "알람 \(indexPath.row + 1)"
+        return cell
+    }
+}
+
+extension AlarmPageVC: UITableViewDelegate {
+    
 }
