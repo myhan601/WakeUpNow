153d593a6d09519168a1b706d651701759ecac39
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-05-13 15:07:52 +0900

    AlarmPage create

---
+++ WakeUpNow/AlarmPageVC.swift
@@ -0,0 +1,29 @@
+//
+//  AlarmPageVC.swift
+//  WakeUpNow
+//
+//  Created by 한철희 on 5/13/24.
+//
+
+import UIKit
+
+class AlarmPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
