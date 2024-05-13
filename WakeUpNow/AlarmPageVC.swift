a7e8f9081fac15013e181e0304d081d3dbc32861
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-05-13 15:24:03 +0900

    SnapKit 문제 해결 및 화면 테스트

--- WakeUpNow/AlarmPageVC.swift
+++ WakeUpNow/AlarmPageVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class AlarmPageVC: UIViewController {
     
@@ -14,19 +15,27 @@ class AlarmPageVC: UIViewController {
         
         view.backgroundColor = .white
         
+        // 레이블 생성
         let label = UILabel()
         
         // 레이블에 표시될 텍스트 설정
         label.text = "안녕하세요, 레이블입니다!"
-        
-        // 레이블의 위치와 크기 설정
-        label.frame = CGRect(x: 20, y: 20, width: 300, height: 20)
         
         // 레이블의 텍스트 정렬 설정
         label.textAlignment = .center
         
         // 레이블을 뷰에 추가
         self.view.addSubview(label)
+        
+        // SnapKit을 사용하여 레이블의 위치와 크기 설정
+        label.snp.makeConstraints { (make) in
+            // 화면 중앙에 오도록 설정
+            make.center.equalToSuperview()
+            
+            // 레이블의 너비와 높이 설정 (필요한 경우)
+            make.width.equalTo(300)
+            make.height.equalTo(20)
+        }
     }
     
     
