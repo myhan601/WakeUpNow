a53bce2816097f4ab1f5633384328c4e7260d756
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-05-16 17:42:26 +0900

    first commit & create TabBarVC

---
+++ WakeUpNow/Controller/TabBar/TabBarVC.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarVC.swift
+//  WakeUpNow
+//
+//  Created by 한철희 on 5/16/24.
+//
+
+import UIKit
+
+class TabBarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
