e060fdb2d22f4f10c69aeb826a70f1b94fcb5321
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-05-16 17:57:14 +0900

    [Feat]#9-탭바 순서 변경

--- WakeUpNow/Controller/TabBar/TabBarVC.swift
+++ WakeUpNow/Controller/TabBar/TabBarVC.swift
@@ -11,27 +11,27 @@ class TabBarVC: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // TimerViewController 설정
-        let timerVC = TimerViewController()
-        let timerNav = UINavigationController(rootViewController: timerVC)
-        timerNav.tabBarItem.title = "타이머"
-        timerNav.tabBarItem.image = UIImage(systemName: "timer") // 시스템 아이콘 사용, 원하는 이미지로 변경 가능
-
-        // StopWatchViewController 설정
-        let stopWatchVC = StopWatchViewController()
-        let stopWatchNav = UINavigationController(rootViewController: stopWatchVC)
-        stopWatchNav.tabBarItem.title = "스톱워치"
-        stopWatchNav.tabBarItem.image = UIImage(systemName: "stopwatch") // 시스템 아이콘 사용, 원하는 이미지로 변경 가능
-
+        
         // AlarmPageVC 설정
         let alarmPageVC = AlarmPageVC()
         let alarmPageNav = UINavigationController(rootViewController: alarmPageVC)
         alarmPageNav.tabBarItem.title = "알람"
         alarmPageNav.tabBarItem.image = UIImage(systemName: "alarm") // 시스템 아이콘 사용, 원하는 이미지로 변경 가능
 
+        // StopWatchViewController 설정
+        let stopWatchVC = StopWatchViewController()
+        let stopWatchNav = UINavigationController(rootViewController: stopWatchVC)
+        stopWatchNav.tabBarItem.title = "스톱워치"
+        stopWatchNav.tabBarItem.image = UIImage(systemName: "stopwatch") // 시스템 아이콘 사용, 원하는 이미지로 변경 가능
+        
+        // TimerViewController 설정
+        let timerVC = TimerViewController()
+        let timerNav = UINavigationController(rootViewController: timerVC)
+        timerNav.tabBarItem.title = "타이머"
+        timerNav.tabBarItem.image = UIImage(systemName: "timer") // 시스템 아이콘 사용, 원하는 이미지로 변경 가능
+
         // 탭바 컨트롤러에 뷰 컨트롤러 설정
-        viewControllers = [timerNav, stopWatchNav, alarmPageNav]
+        viewControllers = [alarmPageNav, stopWatchNav, timerNav]
     }
 }
 
