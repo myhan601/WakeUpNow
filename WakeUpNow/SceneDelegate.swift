cf37e9158c11b8ce541cf2a66d281837e903da29
yoonhee <parkyunhee1217@gmail.com>
2024-05-17 10:13:30 +0900

    Merge branch 'develop'

--- WakeUpNow/SceneDelegate.swift
+++ WakeUpNow/SceneDelegate.swift
@@ -20,13 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        
-        let mainVC = AlarmPageVC()
-        
-        let navigationController = UINavigationController(rootViewController: mainVC)
-        
-        window.rootViewController = navigationController
-        
+        window.rootViewController = TabBarVC()
         window.makeKeyAndVisible()
         
         self.window = window
