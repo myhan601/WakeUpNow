0e8e591287745c5bffa18ab86bb193f253ae9e90
swimmmmmmm <khan991117@gmail.com>
2024-05-21 14:37:58 +0900

    [feat] color palette 색상 추가
    .

--- WakeUpNow/Utility/ColorPalette.swift
+++ WakeUpNow/Utility/ColorPalette.swift
@@ -2,13 +2,14 @@
 import UIKit
 
 enum ColorPalette {
-    static let darkNavy  = UIColor(cgColor: CGColor(red: 30, green: 38, blue: 41, alpha: 1.0))
-    static let primary   = UIColor(cgColor: CGColor(red: 244, green: 241, blue: 236, alpha: 1.0))
-    static let second    = UIColor(cgColor: CGColor(red: 230, green: 224, blue: 212, alpha: 1.0))
-    static let error     = UIColor(cgColor: CGColor(red: 109, green: 66, blue: 54, alpha: 1.0))
-    static let success   = UIColor(cgColor: CGColor(red: 33, green: 107, blue: 136, alpha: 1.0))
-}
-
-extension UIColor {
-    static let s = UIColor(cgColor: CGColor(gray: 12, alpha: 0.1))
+    static let wakeDeepNavy  = UIColor(cgColor: CGColor(red: 30, green: 38, blue: 41, alpha: 1.0))
+    static let wakeDarkGray   = UIColor(cgColor: CGColor(red: 84, green: 89, blue: 90, alpha: 1.0))
+    static let wakeGray    = UIColor(cgColor: CGColor(red: 137, green: 139, blue: 138, alpha: 1.0))
+    static let wakeLightGray     = UIColor(cgColor: CGColor(red: 191, green: 190, blue: 187, alpha: 1.0))
+    static let wakeBrightGray   = UIColor(cgColor: CGColor(red: 220, green: 218, blue: 214, alpha: 1.0))
+    static let wakeLightBeige   = UIColor(cgColor: CGColor(red: 244, green: 241, blue: 236, alpha: 1.0))
+    static let wakeBeige   = UIColor(cgColor: CGColor(red: 230, green: 224, blue: 212, alpha: 1.0))
+    static let wakeRed   = UIColor(cgColor: CGColor(red: 190, green: 66, blue: 54, alpha: 1.0))
+    static let wakeBlue   = UIColor(cgColor: CGColor(red: 33, green: 107, blue: 136, alpha: 1.0))
+    static let wakeLightSky   = UIColor(cgColor: CGColor(red: 189, green: 200, blue: 204, alpha: 1.0))
 }
