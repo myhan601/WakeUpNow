578fef31db919524621f19883a295163617147c5
swimmmmmmm <khan991117@gmail.com>
2024-05-21 14:05:41 +0900

    [refactor] 재설정 문구 수정
    .

---
+++ WakeUpNow/Utility/ColorPalette.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+enum ColorPalette {
+    static let darkNavy  = UIColor(cgColor: CGColor(red: 30, green: 38, blue: 41, alpha: 1.0))
+    static let primary   = UIColor(cgColor: CGColor(red: 244, green: 241, blue: 236, alpha: 1.0))
+    static let second    = UIColor(cgColor: CGColor(red: 230, green: 224, blue: 212, alpha: 1.0))
+    static let error     = UIColor(cgColor: CGColor(red: 109, green: 66, blue: 54, alpha: 1.0))
+    static let success   = UIColor(cgColor: CGColor(red: 33, green: 107, blue: 136, alpha: 1.0))
+}
+
+extension UIColor {
+    static let s = UIColor(cgColor: CGColor(gray: 12, alpha: 0.1))
+}
