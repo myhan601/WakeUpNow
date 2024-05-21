aba7d6f9956a35d6a05a87dba89a1600c92ea004
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-05-21 22:25:37 +0900

    [Feat]#22-탭바 배경색 변경

--- WakeUpNow/Controller/TabBar/TabBarVC.swift
+++ WakeUpNow/Controller/TabBar/TabBarVC.swift
@@ -36,6 +36,8 @@ class TabBarVC: UITabBarController {
         // 탭 바의 색상 설정
         UITabBar.appearance().tintColor = ColorPalette.wakeRed // 선택된 아이템의 색상
         UITabBar.appearance().unselectedItemTintColor = UIColor.black // 선택되지 않은 아이템의 색상
+        
+        UITabBar.appearance().backgroundColor = ColorPalette.wakeBeige // 여기서 원하는 색상으로 설정
     }
 }
 
