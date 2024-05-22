cb23f587a86774068ab96efd4bcc06a8b08d544e
hyelim9300 <hye930011@naver.com>
2024-05-22 17:57:44 +0900

    Fix : 타이머 사운드 설정 기능 수정, 로컬 알림 추가

--- WakeUpNow/Controller/Timer/SoundSettingViewController.swift
+++ WakeUpNow/Controller/Timer/SoundSettingViewController.swift
@@ -5,103 +5,47 @@
 //  Created by 서혜림 on 5/20/24.
 //
 
+
 import UIKit
 
 protocol SoundSettingDelegate: AnyObject {
     func setAlarmSound(named soundName: String)
 }
 
-class SoundSettingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class SoundSettingViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    var selectedSound: String = "Alarm"
-    var soundOptions: [String] = []
     weak var delegate: SoundSettingDelegate?
     
-    lazy var tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        tableView.layer.cornerRadius = 10
-        tableView.clipsToBounds = true
-        return tableView
-    }()
+    let soundOptions = ["Alarm", "Apex", "Ascending", "Bark", "Beacon", "Bell Tower", "Blues", "Boing", "Bullentin", "By The Seaside", "Chimes", "Circuit"]
+    
+    private let tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .white
-        setupSubviews()
-        setupConstraints()
-        loadSoundOptions()
-        tableView.reloadData()
-    }
-    
-    private func setupSubviews() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        tableView.frame = self.view.bounds
         view.addSubview(tableView)
     }
     
-    private func setupConstraints() {
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
-        ])
-    }
-    
-    private func loadSoundOptions() {
-        let bundlePath = Bundle.main.bundlePath
-        let ringtonesPath = (bundlePath as NSString).appendingPathComponent("Ringtones")
-        let ringtonesURL = URL(fileURLWithPath: ringtonesPath)
-        
-        do {
-            let contents = try FileManager.default.contentsOfDirectory(at: ringtonesURL, includingPropertiesForKeys: nil, options: [])
-            soundOptions = contents.filter { $0.pathExtension == "mp3" }.map { $0.deletingPathExtension().lastPathComponent }
-            print("Sound options: \(soundOptions)") // 디버깅용
-        } catch {
-            print("Ringtones 폴더 내용을 읽는 중 에러 발생: \(error.localizedDescription)")
-        }
-    }
-
-    // MARK: - UITableViewDataSource Methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return soundOptions.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = soundOptions[indexPath.row]
-        cell.backgroundColor = ColorPalette.wakeBeige
-        cell.textLabel?.textColor = ColorPalette.wakeDeepNavy
-        
-        if indexPath.row == 0 {
-            cell.roundCorners(corners: [.topLeft, .topRight], radius: 10.0)
-        } else if indexPath.row == soundOptions.count - 1 {
-            cell.roundCorners(corners: [.bottomLeft, .bottomRight], radius: 10.0)
-        } else {
-            cell.roundCorners(corners: [], radius: 0.0)
-        }
-        
         return cell
     }
-
-    // MARK: - UITableViewDelegate Methods
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let soundName = soundOptions[indexPath.row]
-        selectedSound = soundName
-        delegate?.setAlarmSound(named: soundName)
+        tableView.deselectRow(at: indexPath, animated: true)
+        let selectedSound = soundOptions[indexPath.row]
+        delegate?.setAlarmSound(named: selectedSound)
         dismiss(animated: true, completion: nil)
-    }
-}
-
-extension UIView {
-    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
-        DispatchQueue.main.async {
-            let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-            let mask = CAShapeLayer()
-            mask.path = path.cgPath
-            self.layer.mask = mask
-        }
     }
 }
--- WakeUpNow/Controller/Timer/TimerViewController.swift
+++ WakeUpNow/Controller/Timer/TimerViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 import AVFoundation
+import UserNotifications
 
-class TimerViewController: UIViewController, TimeSettingDelegate, SoundSettingDelegate {
+class TimerViewController: UIViewController, TimeSettingDelegate, SoundSettingDelegate, UNUserNotificationCenterDelegate {
     
     // MARK: - Properties
     private var timer = Timer()
@@ -21,7 +22,7 @@ class TimerViewController: UIViewController, TimeSettingDelegate, SoundSettingDe
     private var startTime: Date?
     private var endTime: Date?
     private var alarmPlayer: AVAudioPlayer?
-    private var selectedSound: String = "Alarm.mp3"
+    private var selectedSound: String = "Alarm"
     
     // UI Components
     lazy var timerStartBtn: UIButton = mainBtn(title: "시작", action: #selector(startTimer))
@@ -73,7 +74,7 @@ class TimerViewController: UIViewController, TimeSettingDelegate, SoundSettingDe
         
         return button
     }()
-
+    
     
     lazy var soundNameLabel: UILabel = {
         let label = UILabel()
@@ -92,10 +93,26 @@ class TimerViewController: UIViewController, TimeSettingDelegate, SoundSettingDe
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = ColorPalette.wakeLightBeige
-//        timerStartBtn.setTitleColor(.ColorPalette.wakeLightBeige, for: .normal)
+        timerStartBtn.setTitleColor(ColorPalette.wakeRed, for: .normal)
         timerStartBtn.titleLabel?.font = .boldSystemFont(ofSize: 20)
         setupSubviews()
         setupConstraints()
+        setupAudioSession()
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+                    DispatchQueue.main.async {
+                        if granted {
+                            print("사용자가 알림을 허용했습니다.")
+                        } else {
+                            print("사용자가 알림을 허용하지 않았습니다.")
+                        }
+                    }
+                }
+        
+        UNUserNotificationCenter.current().delegate = self
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(appDidEnterBackground), name: UIApplication.didEnterBackgroundNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(appWillEnterForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -156,19 +173,21 @@ class TimerViewController: UIViewController, TimeSettingDelegate, SoundSettingDe
         view.bringSubviewToFront(countDownLabel)
     }
     
+    // 주요 버튼 외형
     private func mainBtn(title: String, action: Selector, isEnabled: Bool = true) -> UIButton {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle(title, for: .normal)
         button.titleLabel?.font = .systemFont(ofSize: 20)
         button.backgroundColor = isEnabled ? ColorPalette.wakeBeige : ColorPalette.wakeLightGray
-        button.setTitleColor(isEnabled ? ColorPalette.wakeDeepNavy : .gray, for: .normal)
+        button.setTitleColor(isEnabled ? ColorPalette.wakeDeepNavy : ColorPalette.wakeGray, for: .normal)
         button.setTitleColor(.white, for: .highlighted)
         button.isEnabled = isEnabled
         button.addTarget(self, action: action, for: .touchUpInside)
         return button
     }
     
+    // 서브 버튼 외형
     private func subBtn(title: String, action: Selector, isEnabled: Bool = true) -> UIButton {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -182,18 +201,20 @@ class TimerViewController: UIViewController, TimeSettingDelegate, SoundSettingDe
         return button
     }
     
+    // 카운트 다운 타임 레이블 외형
     private func timeLabel(text: String) -> UILabel {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = ColorPalette.wakeGray
         label.font = .systemFont(ofSize: 65)
         label.text = text
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didSelectTimeTapped))
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(timeSetting))
         label.addGestureRecognizer(tapGestureRecognizer)
         label.isUserInteractionEnabled = true
         return label
     }
     
+    // 서브 레이블 외형
     private func subLabel(text: String) -> UILabel {
         let label = UILabel()
         label.textColor = ColorPalette.wakeGray
@@ -203,7 +224,8 @@ class TimerViewController: UIViewController, TimeSettingDelegate, SoundSettingDe
         return label
     }
     
-    @objc func didSelectTimeTapped() {
+    // 시간 설정
+    @objc func timeSetting() {
         let timeSettingVC = TimeSettingViewController()
         timeSettingVC.delegate = self
         timeSettingVC.modalPresentationStyle = .pageSheet
@@ -215,6 +237,7 @@ class TimerViewController: UIViewController, TimeSettingDelegate, SoundSettingDe
         present(timeSettingVC, animated: true, completion: nil)
     }
     
+    // 설정한 시간 업데이트
     func didSelectTime(hours: Int, minutes: Int, seconds: Int) {
         selectedHours = hours
         selectedMinutes = minutes
@@ -226,6 +249,7 @@ class TimerViewController: UIViewController, TimeSettingDelegate, SoundSettingDe
         isTimerSet = true
     }
     
+    // 타이머 '시작' 버튼
     @objc func startTimer() {
         guard isTimerSet else {
             let alertController = UIAlertController(title: "알림", message: "타이머를 설정해주세요.", preferredStyle: .alert)
@@ -233,20 +257,27 @@ class TimerViewController: UIViewController, TimeSettingDelegate, SoundSettingDe
             present(alertController, animated: true, completion: nil)
             return
         }
-        
+        // 카운트 다운 진행 중일 경우 버튼 외형 및 기능 변경
         if isTimerRunning {
             timer.invalidate()
             isTimerRunning = false
             timerStartBtn.setTitle("재개", for: .normal)
+            timerStartBtn.setTitleColor(ColorPalette.wakeRed, for: .normal)
+            timerStartBtn.backgroundColor = ColorPalette.wakeBeige
         } else {
             if remainingTimeInSeconds <= 0 {
                 remainingTimeInSeconds = selectedHours * 3600 + selectedMinutes * 60 + selectedSeconds
             }
-            setupTimer(with: Double(remainingTimeInSeconds))
-            calculateEndTime()
-            timerStartBtn.setTitle("일시정지", for: .normal)
-            isTimerRunning = true
+            if remainingTimeInSeconds > 0 {
+                setupTimer(with: Double(remainingTimeInSeconds))
+                calculateEndTime()
+                timerStartBtn.setTitle("일시정지", for: .normal)
+                timerStartBtn.setTitleColor(ColorPalette.wakeBlue, for: .normal)
+                timerStartBtn.backgroundColor = ColorPalette.wakeLightSky
+                isTimerRunning = true
+            }
         }
+        // 취소 버튼 활성화
         timerCancelBtn.isEnabled = true
         timerCancelBtn.setTitleColor(ColorPalette.wakeRed, for: .normal)
     }
@@ -254,6 +285,7 @@ class TimerViewController: UIViewController, TimeSettingDelegate, SoundSettingDe
     @objc func cancelTimer() {
         timer.invalidate()
         resetTimerLabel()
+        countDownLabel.textColor = ColorPalette.wakeLightGray
         selectedHours = 0
         selectedMinutes = 0
         selectedSeconds = 0
@@ -261,11 +293,14 @@ class TimerViewController: UIViewController, TimeSettingDelegate, SoundSettingDe
         isTimerSet = false
         isTimerRunning = false
         timerStartBtn.setTitle("시작", for: .normal)
+        timerStartBtn.setTitleColor(ColorPalette.wakeRed, for: .normal)
+        timerStartBtn.backgroundColor = ColorPalette.wakeBeige
         timerCancelBtn.isEnabled = false
         timerCancelBtn.setTitleColor(.lightGray, for: .normal)
         
         circularTimerView.resetProgress()
         endTimeLabel.text = "탭하여 설정"
+        alarmPlayer?.stop()
     }
     
     private func setupTimer(with totalSeconds: Double) {
@@ -308,6 +343,7 @@ class TimerViewController: UIViewController, TimeSettingDelegate, SoundSettingDe
     
     private func timerDidFinish() {
         playAlarmSound()
+        scheduleLocalNotification()
         
         let alertController = UIAlertController(title: "타이머 종료", message: "설정한 시간이 종료되었습니다.", preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "확인", style: .default) { [weak self] _ in
@@ -322,7 +358,8 @@ class TimerViewController: UIViewController, TimeSettingDelegate, SoundSettingDe
     }
     
     private func playAlarmSound() {
-        guard let soundURL = Bundle.main.url(forResource: selectedSound, withExtension: "mp3", subdirectory: "Ringtones") else {
+        let soundName = selectedSound.isEmpty ? "Alarm" : selectedSound
+        guard let soundURL = Bundle.main.url(forResource: soundName, withExtension: "mp3") else {
             print("사운드 파일을 찾을 수 없습니다.")
             return
         }
@@ -333,6 +370,16 @@ class TimerViewController: UIViewController, TimeSettingDelegate, SoundSettingDe
             alarmPlayer?.play()
         } catch {
             print("사운드 파일을 재생할 수 없습니다: \(error.localizedDescription)")
+        }
+    }
+    
+    private func setupAudioSession() {
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(.playback, mode: .default)
+            try audioSession.setActive(true)
+        } catch {
+            print("오디오 세션 설정 오류: \(error.localizedDescription)")
         }
     }
     
@@ -361,6 +408,64 @@ class TimerViewController: UIViewController, TimeSettingDelegate, SoundSettingDe
     func setAlarmSound(named soundName: String) {
         self.selectedSound = soundName
         soundNameLabel.text = soundName
+    }
+    
+    @objc func appDidEnterBackground() {
+        if isTimerRunning {
+            UserDefaults.standard.set(Date(), forKey: "startTime")
+            UserDefaults.standard.set(remainingTimeInSeconds, forKey: "remainingTimeInSeconds")
+            UserDefaults.standard.set(isTimerRunning, forKey: "isTimerRunning")
+            UserDefaults.standard.set(isTimerSet, forKey: "isTimerSet")
+        }
+    }
+    
+    @objc func appWillEnterForeground() {
+        if let startTime = UserDefaults.standard.value(forKey: "startTime") as? Date,
+           let savedRemainingTimeInSeconds = UserDefaults.standard.value(forKey: "remainingTimeInSeconds") as? Int,
+           let savedIsTimerRunning = UserDefaults.standard.value(forKey: "isTimerRunning") as? Bool,
+           let savedIsTimerSet = UserDefaults.standard.value(forKey: "isTimerSet") as? Bool {
+            
+            let elapsedTime = Int(Date().timeIntervalSince(startTime))
+            remainingTimeInSeconds = savedRemainingTimeInSeconds - elapsedTime
+            
+            if remainingTimeInSeconds <= 0 {
+                timerDidFinish()
+            } else {
+                isTimerRunning = savedIsTimerRunning
+                isTimerSet = savedIsTimerSet
+                setupTimer(with: Double(remainingTimeInSeconds))
+            }
+        }
+    }
+    
+    private func scheduleLocalNotification() {
+        if remainingTimeInSeconds > 0 {
+            let content = UNMutableNotificationContent()
+            content.title = "타이머 종료"
+            content.body = "설정한 시간이 종료되었습니다."
+            content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "\(selectedSound).mp3"))
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(remainingTimeInSeconds), repeats: false)
+            let request = UNNotificationRequest(identifier: "timerFinished", content: content, trigger: trigger)
+            
+            UNUserNotificationCenter.current().add(request) { (error) in
+                if let error = error {
+                    print("알림 요청 에러: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        if #available(iOS 14.0, *) {
+            completionHandler([.banner, .sound, .badge])
+        } else {
+            completionHandler([.alert, .sound, .badge])
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
     }
 }
 
